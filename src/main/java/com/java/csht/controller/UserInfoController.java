bbf92168749c288a79ec89519e6c2c65ff5818d0
lienit <1184144908@qq.com>
2019-10-28 11:17:59 +0800

    校园二手交易系统实现用户管理界面绑定邮箱(未实现手机发送验证码）

--- src/main/java/com/java/csht/controller/UserInfoController.java
+++ src/main/java/com/java/csht/controller/UserInfoController.java
@@ -17,7 +17,7 @@ public class UserInfoController {
 
     @RequestMapping("/userinfo")
     public ModelAndView UserInfo(){
-        ModelAndView modelAndView = new ModelAndView("/userInfo");
+        ModelAndView modelAndView = new ModelAndView("userInfo");
 
         Integer count = 0;
 
@@ -56,10 +56,10 @@ public class UserInfoController {
 
         return modelAndView;
     }
-
+//修改绑定手机
     @RequestMapping("/bindmobile")
     public ModelAndView BindMobile(String userId){
-        ModelAndView modelAndView = new ModelAndView("/userInfo/bindmobile");
+        ModelAndView modelAndView = new ModelAndView("userInfo/bindMobile");
         User user = userServier.findById(userId);
         String mobile = user.getMobile();
         Integer id = user.getId();
@@ -89,7 +89,77 @@ public class UserInfoController {
     @PostMapping("/editbindmobileiolp")
     @ResponseBody
     public String EditBindMobile(String mobile,String verify,Integer userId){
-        System.out.println(mobile);
         return "success";
+    }
+//修改登录密码
+    @RequestMapping("/editpsw")
+    public ModelAndView editPsw(String userId){
+        ModelAndView modelAndView = new ModelAndView("userInfo/editPsw");
+        User user = userServier.findById(userId);
+        String mailbox = user.getMailbox();
+        String mobile = user.getMobile();
+        //截取邮箱,隐藏邮箱
+        if (!mailbox.equals("")){
+
+            String[] split = mailbox.split("@");
+            Integer length = split[0].length()-3;
+            String mailbox1 = split[0].substring(0,3);
+            String mailbox2 = split[0].substring(length);
+            user.setMailbox(mailbox1+"*******"+mailbox2+"@"+split[1]);
+        }
+
+//        截取手机号，隐藏手机号
+        if (!mobile.equals("")){
+            Integer mobilelen = mobile.length()-3;
+            String mobile1 = mobile.substring(0, 3);
+            String mobile2 = mobile.substring(mobilelen);
+            user.setMobile(mobile1+"*******"+mobile2);
+
+        }
+        modelAndView.addObject("user", user);
+
+        return modelAndView;
+    }
+    @PostMapping("/pswverify")
+    @ResponseBody
+    public String PswVerify(String userId, String editMode){
+        System.out.println(userId);
+
+        return "error";
+    }
+
+    @PostMapping("/pswedit")
+    @ResponseBody
+    public String PswEdit(){
+        return "error";
+    }
+
+//    修改绑定邮箱
+    @RequestMapping("/editmailbox")
+    public ModelAndView EditMailbox(String userId){
+        ModelAndView modelAndView = new ModelAndView("userInfo/editMailbox");
+        User user = userServier.findById(userId);
+        String mailbox = user.getMailbox();
+        String mobile = user.getMobile();
+        //截取邮箱,隐藏邮箱
+        if (!mailbox.equals("")){
+
+            String[] split = mailbox.split("@");
+            Integer length = split[0].length()-3;
+            String mailbox1 = split[0].substring(0,3);
+            String mailbox2 = split[0].substring(length);
+            user.setMailbox(mailbox1+"*******"+mailbox2+"@"+split[1]);
+        }
+
+//        截取手机号，隐藏手机号
+        if (!mobile.equals("")){
+            Integer mobilelen = mobile.length()-3;
+            String mobile1 = mobile.substring(0, 3);
+            String mobile2 = mobile.substring(mobilelen);
+            user.setMobile(mobile1+"*******"+mobile2);
+
+        }
+        modelAndView.addObject("user", user);
+        return modelAndView;
     }
 }
